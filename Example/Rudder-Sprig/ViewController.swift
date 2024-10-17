661818cab86fa48554f3a08dbfac863a8ca0bee2
Abhishek Pandey <64667840+1abhishekpandey@users.noreply.github.com>
2024-10-17 12:58:58 +0530

    chore: add swift based ViewController

---
+++ Example/Rudder-Sprig/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Rudder-Sprig_Example
+//
+//  Created by Abhishek Pandey on 16/10/24.
+//  Copyright © 2024 arnabp92. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
