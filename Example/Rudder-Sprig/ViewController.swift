eeaacbbd96de915df62aa9e862cc4f570ca3bd73
Abhishek Pandey <64667840+1abhishekpandey@users.noreply.github.com>
2024-10-17 20:01:56 +0530

    chore: update sample app

--- Example/Rudder-Sprig/ViewController.swift
+++ Example/Rudder-Sprig/ViewController.swift
@@ -22,6 +22,14 @@ class ViewController: UIViewController {
     }
     
     @IBAction func identify(_ sender: Any) {
+        let traits: [String: Any] = [
+            "email": "test@mail.com",
+            "key-1": "value",
+            "key-2": 100,
+            "key-3": 122.56,
+            "key-4": true,
+        ]
+        RSClient.getInstance().identify("User iOS 1", traits: traits)
     }
     
     @IBAction func trackWithoutProperties(_ sender: Any) {
@@ -30,12 +38,13 @@ class ViewController: UIViewController {
     
     @IBAction func trackWithProperties(_ sender: Any) {
         let properties: [String: Any] = [
-            "key-1": "value",
-            "key-2": 100,
+            "key-5": "value 5",
+            "key-6": 200,
         ]
         RSClient.getInstance().track("Track event with properties", properties: properties)
     }
     
     @IBAction func reset(_ sender: Any) {
+        RSClient.getInstance().reset(false)
     }
 }
